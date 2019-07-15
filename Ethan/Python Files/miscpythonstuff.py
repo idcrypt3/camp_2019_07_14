70a409816ab63b67be6bd875845e0e531227d8f3
idcrypt3 <idcrypt3@gmail.com>
2019-07-15 11:24:10 -0700

    EthanZ
    added Userstory and misc python lessons

---
+++ Ethan/Python Files/miscpythonstuff.py
@@ -0,0 +1,9 @@
+import random
+
+list_of_numbers = [0, 4, 5, 9, 7, 8, 1, 2, 6, 3]
+list_of_strings = ["Blue feather", "Yellow feather", "Orange feather", "Gray feather"]
+
+list_of_numbers.sort()
+list_of_numbers.pop()
+list_of_strings.remove("Blue feather")
+print(list_of_numbers, list_of_strings)
---
+++ Ethan/Python Files/userstory.py
@@ -0,0 +1,14 @@
+password = input("Whats the password")
+
+if password == "test":
+    print("The dragon lived by the mountain.")
+    print("He was red and black with scales.")
+    print("People thought the dragon was too loud and eating the livestock.")
+    print("The people sent Geoff, the village's butcher, to fight the dragon.")
+    print("The dragon thought that it would be s simple nuisance.")
+    print("he was right.")
+if password != "test":
+    print("Are you ready kids?")
+    print("Aye-aye captain.")
+    print("I can't hear you...")
+    print("Aye-aye captain!")
