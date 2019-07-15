ec2276159ceb71d249870a4fe74ac4a55826bc34
idCrypt3 <idcrypt3@gmail.com>
2019-07-15 14:30:13 -0700

    noahaddintro.py

---
+++ noah/intro.py
@@ -0,0 +1,14 @@
+import this
+print("hello world")
+print("what? i can type anything?!")
+print("hello! how wre you?")
+print(4 + 5)
+five=input("whats your name ")
+print (five) (noah)
+my_coins= input (8000+5)
+string_looks_like_a_number = input ("enter s number.")
+print (string_looks_like_a_sting)
+my_multiple_line_string = """This is the first line 
+this is the second line 
+this is the third line"""
+isinstance(five_noah,type)
