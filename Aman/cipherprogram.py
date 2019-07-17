19de130ba91beb3f71369a4efad7ffa47d84757c
iDCrypt3 <iDCrypt3@gmail.com>
2019-07-17 10:33:13 -0700

    Aman- Files for Crypt.io Refactored

--- Aman/cipherprogram.py
+++ Aman/cipherprogram.py
@@ -1,34 +1,39 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-partialOne = ""
-partialTwo = ""
-newAlphabet = ""
+def main():
+    message = input("Please enter a secret message: ").lower()
+    key = int(input("Please enter a number to shift by: "))
+    print(ceasercipher(message, key))
 
-message = input("Please enter a secret message: ").lower()
-key = int(input("Please enter a number to shift by: "))
+def ceasercipher(message, key):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    partialOne = ""
+    partialTwo = ""
+    newAlphabet = ""
 
-if key == 0:
-    newAlphabet = alphabet
+    if key == 0:
+        newAlphabet = alphabet
 
-elif key > 0:
-    partialOne = alphabet[:key]
-    partialTwo = alphabet[key:]
-    newAlphabet = partialTwo + partialOne
-
-else:
-    partialOne = alphabet[:(26 + key)]
-    partialTwo = alphabet[(26 + key):]
-    newAlphabet = partialTwo + partialOne
-
-newMessage = ""
-
-for i in range(0, len(message)):
-    index = alphabet.find(message[i])
-
-    if index < 0:
-        newMessage += message[i]
+    elif key > 0:
+        partialOne = alphabet[:key]
+        partialTwo = alphabet[key:]
+        newAlphabet = partialTwo + partialOne
 
     else:
-        newMessage += newAlphabet[index]
+        partialOne = alphabet[:(26 + key)]
+        partialTwo = alphabet[(26 + key):]
+        newAlphabet = partialTwo + partialOne
 
-print(newMessage)
+    newMessage = ""
 
+    for i in range(0, len(message)):
+        index = alphabet.find(message[i])
+
+        if index < 0:
+            newMessage += message[i]
+
+        else:
+            newMessage += newAlphabet[index]
+
+    return newMessage
+
+if __name__ == "__main__":
+    main()
