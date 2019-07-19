dd1b85d98768337b4774e6808a9dafeb6bb2cf4a
idCrypt3 <idcrypt3@gmail.com>
2019-07-19 11:21:01 -0700

    somethings wrong noah

--- noah/cipher.py
+++ noah/cipher.py
@@ -1,5 +1,30 @@
-enterpassword = input("whats the password ")
-if enterpassword =="tgq":
-    print("e f g h i j k l m n o p q r s t u v w x y z a b c d")
+alphabet = "abcefghijklmnopqrstuvwxyz"
+partialone = ""
+partialtwo = ""
+newalphabet = ""
+
+
+message = input("please enter a secret message:")
+key = int(input("please enter a number"))
+
+
+if key == 0:
+    newAlphabet = alphabet
+elif key > 0:
+    partialOne = alphabet [:key]
+    partialTwo = alphabet [key:]
+    newAlphabet = partialTwo + partialOne
 else:
-    print("sorry")
+    partialOne = alphabet[:(26 + key)]
+    partialTwo = alphabet[(26 + key):]
+    newAlphabet = partialTwo + partialOne
+
+newMessage = ""
+for i in range(0,len(message)):
+    index = alphabet.find(message[i])
+    if index < 0:
+        newMessage += message[i]
+    else:
+        newMessage += newAlphabet[index]
+
+print(newMessage)
